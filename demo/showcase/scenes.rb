dac616497e2e597e77d3de5f579ead8b76402b98
UK <uk@Macmini-UK.local>
2015-07-14 23:53:15 +0900

    showcase framework

--- demo/showcase/scenes.rb
+++ demo/showcase/scenes.rb
@@ -44,6 +44,55 @@ class GridScene < Scene
 
 end
 
+class TriangleScene < Scene
+
+  def initialize(name)
+    super
+    @time = 0.0
+  end
+
+  def render(vg, width, height, dt = 0.0)
+    @time += dt
+    hue = Math.sin(@time * 0.12)
+    x = width - width/2.0
+    y = height - height/2.0
+
+    nvgSave(vg)
+
+    nvgStrokeWidth(vg, 2.0)
+    nvgStrokeColor(vg, nvgRGBA(255,255,255,192))
+
+    nvgTranslate(vg, x,y)
+    nvgRotate(vg, hue*Math::PI*2)
+
+    r1 = (width < height ? width : height) * 0.5 - 5.0
+    r0 = r1 - 20.0
+    r = r0 - 6
+    ax = Math.cos(120.0/180.0*Math::PI) * r
+    ay = Math.sin(120.0/180.0*Math::PI) * r
+    bx = Math.cos(-120.0/180.0*Math::PI) * r
+    by = Math.sin(-120.0/180.0*Math::PI) * r
+
+    nvgBeginPath(vg)
+    nvgMoveTo(vg, r,0)
+    nvgLineTo(vg, ax,ay)
+    nvgLineTo(vg, bx,by)
+    nvgClosePath(vg)
+    paint = nvgLinearGradient(vg, r,0, ax,ay, nvgHSLA(hue,1.0,0.5,255), nvgRGBA(255,255,255,255))
+    nvgFillPaint(vg, paint)
+    nvgFill(vg)
+    paint = nvgLinearGradient(vg, (r+ax)*0.5,(0+ay)*0.5, bx,by, nvgRGBA(0,0,0,0), nvgRGBA(0,0,0,255))
+    nvgFillPaint(vg, paint)
+    nvgFill(vg)
+    nvgStrokeColor(vg, nvgRGBA(0,0,0,64))
+    nvgStroke(vg)
+
+
+    nvgRestore(vg)
+  end
+
+end
+
 class Showcase
 
   def set_viewport_size(w, h)
@@ -57,6 +106,7 @@ class Showcase
     @scene_id = 0
     @scenes = [
       GridScene.new("Grid Scene"),
+      TriangleScene.new("Triangle Scene"),
     ]
   end
 
