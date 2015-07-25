e937b62464a796805df75001315b3664b12dde13
UK <uk@Macmini-UK.local>
2015-07-25 15:58:49 +0900

    hex.rb

--- demo/hex_renderer.rb
+++ demo/hex_renderer.rb
@@ -1,5 +1,6 @@
 require 'opengl'
 require 'glfw'
+# require 'set'
 require_relative '../nanovg'
 require_relative 'hex'
 
@@ -53,7 +54,31 @@ if __FILE__ == $0
   fbWidth_buf  = '        '
   fbHeight_buf = '        '
 
-  hex_grid_layout = Layout.new(Layout::FLAT, Point.new(30, 30), Point.new(1280/2.0, 720/2.0))
+  # Hex map rendering layout
+  hex_grid_layout = Layout.new(Layout::FLAT, Point.new(28, 28), Point.new(1280/2.0, 720/2.0))
+
+  # Hex map storage
+  hex_map = [] # Set.new
+  # Rectangular map loop for Layout::FLAT
+  map_width = 30
+  map_height = 14
+  for q in 0...map_width
+    q_offset = q >> 1 # (r / 2.0).floor
+    for r in (-q_offset)...(map_height-q_offset)
+      hex_map << Hex.new(q, r)
+    end
+  end
+=begin
+  # Rectangular map loop for Layout::POINTY
+  map_width = 25
+  map_height = 16
+  for r in 0...map_height
+    r_offset = r >> 1 # (r / 2.0).floor
+    for q in (-r_offset)...(map_width-r_offset)
+      hex_map << Hex.new(q, r)
+    end
+  end
+=end
 
   while glfwWindowShouldClose( window ) == 0
 
@@ -70,41 +95,35 @@ if __FILE__ == $0
     glClearColor(0.8, 0.8, 0.8, 1.0)
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT)
 
-    hex_grid_layout.origin.x = fbWidth / 2.0
-    hex_grid_layout.origin.y = fbHeight / 2.0
+    hex_grid_layout.origin.x = hex_grid_layout.size.x * 1.1 #fbWidth / 2.0
+    hex_grid_layout.origin.y = hex_grid_layout.size.y * 1.1 #fbHeight / 2.0
 
     nvgBeginFrame(vg, winWidth, winHeight, pxRatio)
     nvgSave(vg)
 
-    n = 12
     nvgStrokeColor(vg, nvgRGBA(32, 64, 128, 255))
-    nvgStrokeWidth(vg, 3.0)
-    (-n..n).each do |q|
-      (-n..n).each do |r|
-        if q + r > n
-          # r = n - q
-          next
-        elsif q + r < -n
-          # r = -n - q
-          next
-        end
-        h = Hex.new(q, r)
-        corners = hex_grid_layout.polygon_corners(h)
-        nvgBeginPath(vg)
-        nvgMoveTo(vg, corners[0].x, corners[0].y)
-        (1..5).each do |i|
-          nvgLineTo(vg, corners[i].x, corners[i].y)
-        end
-        point = hex_grid_layout.hex_to_pixel(h)
-        b = 64 * (point.x >= 0 ? point.x / fbWidth : 0)
-        nvgClosePath(vg)
-        gradient_start = nvgRGBA(164, 192, 192 + b, 255)
-        gradient_end = nvgRGBA(224,255,255,255)
-        paint = nvgLinearGradient(vg, point.x,point.y, point.x+n,point.y+n, gradient_start, gradient_end)
-        nvgFillPaint(vg, paint)
-        nvgFill(vg)
-        nvgStroke(vg)
+    nvgStrokeWidth(vg, 1.5)
+    hex_map.each do |h|
+      center = hex_grid_layout.hex_to_pixel(h)
+      corners = hex_grid_layout.polygon_corners(h)
+      corners = corners.collect {|corner|
+        x = (corner.x - center.x) * 0.9 + center.x
+        y = (corner.y - center.y) * 0.9 + center.y
+        Point.new(x, y)
+      }
+      nvgBeginPath(vg)
+      nvgMoveTo(vg, corners[0].x, corners[0].y)
+      (1..5).each do |i|
+        nvgLineTo(vg, corners[i].x, corners[i].y)
       end
+      b = 64 * (center.x >= 0 ? center.x / fbWidth : 0)
+      nvgClosePath(vg)
+      gradient_start = nvgRGBA(164, 180, 192 + b, 255)
+      gradient_end = nvgRGBA(224,255,255,255)
+      paint = nvgLinearGradient(vg, center.x-10.0,center.y-10.0, center.x+10.0,center.y+10.0, gradient_start, gradient_end)
+      nvgFillPaint(vg, paint)
+      nvgFill(vg)
+      nvgStroke(vg)
     end
     nvgRestore(vg)
     nvgEndFrame(vg)
