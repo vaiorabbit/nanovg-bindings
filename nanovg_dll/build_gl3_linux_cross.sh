b36139923dffdb9787135412762fd7da9d94ecbd
vaiorabbit <vaiorabbit>
2023-02-01 00:33:05 +0900

    WIP Ubuntu Linux (aarch64)

---
+++ nanovg_dll/build_gl3_linux_cross.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+mkdir -p build_gl3
+cd build_gl3
+cmake -D CMAKE_C_FLAGS=-isystem\ /usr/aarch64-linux-gnu/include -D CMAKE_BUILD_TYPE=Release -D CMAKE_C_COMPILER_TARGET=aarch64-linux-gnu -D CMAKE_SYSTEM_PROCESSOR=ARM -D BUILD_SHARED_LIBS=ON -D -D CMAKE_C_COMPILER=clang ../gl3
+make
+export ARCH=aarch64
+cp libnanovg_gl3.so ../../lib/libnanovg_gl3.${ARCH}.so
