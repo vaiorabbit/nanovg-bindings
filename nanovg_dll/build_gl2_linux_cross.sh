6607742c25eff870e9f3628b35bc37317bfa6c7d
vaiorabbit <vaiorabbit>
2023-02-04 19:43:58 +0900

    Update build scripts

--- nanovg_dll/build_gl2_linux_cross.sh
+++ nanovg_dll/build_gl2_linux_cross.sh
@@ -2,6 +2,6 @@
 mkdir -p build_gl2
 cd build_gl2
 cmake -D CMAKE_C_FLAGS=-isystem\ /usr/aarch64-linux-gnu/include -D CMAKE_BUILD_TYPE=Release -D CMAKE_C_COMPILER_TARGET=aarch64-linux-gnu -D CMAKE_SYSTEM_PROCESSOR=ARM -D BUILD_SHARED_LIBS=ON -D CMAKE_C_COMPILER=clang ../gl2
-make
+make VERBOSE=1
 export ARCH=aarch64
 cp libnanovg_gl2.so ../../lib/libnanovg_gl2.${ARCH}.so
