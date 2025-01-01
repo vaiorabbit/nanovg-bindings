ae830ffa1ac6fb51a2bb319bfc7aa4aa1047a444
vaiorabbit <vaiorabbit>
2025-01-01 20:57:20 +0900

    Fixed build scripts

--- nanovg_dll/build_gl3_linux_cross.sh
+++ nanovg_dll/build_gl3_linux_cross.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir -p build_gl3
 cd build_gl3
-cmake -D CMAKE_C_FLAGS=-isystem\ /usr/aarch64-linux-gnu/include -D CMAKE_BUILD_TYPE=Release -D CMAKE_C_COMPILER_TARGET=aarch64-linux-gnu -D CMAKE_SYSTEM_PROCESSOR=ARM -D BUILD_SHARED_LIBS=ON -D CMAKE_C_COMPILER=clang ../gl3
-make
+cmake -D CMAKE_C_FLAGS=-isystem\ /usr/aarch64-linux-gnu/include -D OpenGL_GL_PREFERENCE=GLVND -D CMAKE_BUILD_TYPE=Release -D CMAKE_C_COMPILER_TARGET=aarch64-linux-gnu -D CMAKE_SYSTEM_PROCESSOR=ARM -D BUILD_SHARED_LIBS=ON -D CMAKE_C_COMPILER=clang ../gl3
+cmake --build .
 export ARCH=aarch64
 cp libnanovg_gl3.so ../../lib/libnanovg_gl3.${ARCH}.so
--- nanovg_dll/build_gl3_macos.sh
+++ nanovg_dll/build_gl3_macos.sh
@@ -3,16 +3,16 @@
 #
 mkdir build_gl3_x86_64
 cd build_gl3_x86_64
-export MACOSX_DEPLOYMENT_TARGET=14.0
+export MACOSX_DEPLOYMENT_TARGET=15.0
 cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_OSX_ARCHITECTURES="x86_64" -D BUILD_SHARED_LIBS=ON -D CMAKE_C_COMPILER=clang ../gl3
-make
+cmake --build .
 cp libnanovg_gl3.dylib ../../lib/libnanovg_gl3.x86_64.dylib
 
 cd ..
 
 mkdir build_gl3_arm64
 cd build_gl3_arm64
-export MACOSX_DEPLOYMENT_TARGET=14.0
+export MACOSX_DEPLOYMENT_TARGET=15.0
 cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_OSX_ARCHITECTURES="arm64" -D BUILD_SHARED_LIBS=ON -D CMAKE_C_COMPILER=clang ../gl3
-make
+cmake --build .
 cp libnanovg_gl3.dylib ../../lib/libnanovg_gl3.arm64.dylib
